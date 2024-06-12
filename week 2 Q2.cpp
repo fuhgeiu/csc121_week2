2196acdae33c98f0ef4ac7b472c74587f415884a
alexcarnes <alexcarnes@outlook.com>
2024-06-12 13:07:53 -0500

    update

--- week 2 Q2.cpp
+++ week 2 Q2.cpp
@@ -19,14 +19,14 @@ int main() {
         cout << "enter score\n\n";
 
         while (c) {
-            cin >> score;
-            if (stoi(score) < 0) cout << "grade must be positive number\n";
+        cin >> score;
+        if (stoi(score) < 0) cout << "grade must be positive number\n";
             else break; }
 
         if (score == "exit") break;
 
         if (stoi(score) >= 90) cout << "grade A\n\n";
-        else    det_G(score);
+            else    det_G(score);
 
     }
 
@@ -36,7 +36,7 @@ int main() {
 void det_G (string score) {
 
     if (stoi(score) < 90 && stoi(score) >= 80 ) cout << "grade is B\n\n";
-    else if (stoi(score) < 80 && stoi(score) >= 70 ) cout << "grade is c\n\n";
-    else if (stoi(score) < 70 && stoi(score) >= 60 ) cout << "grade is d\n\n";
-    else cout << "you  failed\n\n";
+        else if (stoi(score) < 80 && stoi(score) >= 70 ) cout << "grade is c\n\n";
+            else if (stoi(score) < 70 && stoi(score) >= 60 ) cout << "grade is d\n\n";
+                else cout << "you  failed\n\n";
 }
