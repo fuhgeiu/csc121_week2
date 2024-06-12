40f1971558dee63c9e9be06f33be286ec1ea5d59
alexcarnes <alexcarnes@outlook.com>
2024-06-12 12:44:04 -0500

    update

--- Q4.cpp
+++ Q4.cpp
@@ -7,17 +7,49 @@ alex carnes 6/6
 
 using namespace std;
 
+void det_G(string);
 
+int main() {
 
+    string score;
+    bool c = 1;
 
+    while (c) {
 
+        cout << "enter score\n\n";
 
-int main () {
+        while (c) {
+            cin >> score;
+            if (stoi(score) < 0) cout << "grade must be positive number\n";
+            else break; }
 
+        if (score == "exit") break;
 
+        det_G(score);
 
-
-
+    }
 
     return 0;
+}
+
+void det_G (string score) {
+
+    int tlscore = score;
+        switch (tlscore) {
+
+            case 9:
+                cout << " letter grade is : A";
+                break;
+
+
+
+
+
+
+
+
+
+        }
+
+
 }
