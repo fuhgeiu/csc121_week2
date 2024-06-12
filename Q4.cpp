480e28c062321b2e471d6ed4726a41dc5bb8f4d3
alexcarnes <alexcarnes@outlook.com>
2024-06-12 12:55:10 -0500

    update

--- Q4.cpp
+++ Q4.cpp
@@ -34,19 +34,50 @@ int main() {
 
 void det_G (string score) {
 
-    int tlscore = score;
+    int tlscore = (stoi(score)/10);
         switch (tlscore) {
 
             case 9:
-                cout << " letter grade is : A";
+                cout << " letter grade is : A\n\n";
                 break;
 
+            case 8:
+                cout << " letter grade is : B\n\n";
+                break;
 
+            case 7:
+                cout << " letter grade is : C\n\n";
+                break;
 
+            case 6:
+                cout << " letter grade is : D\n\n";
+                break;
 
+            case 5:
+                cout << " you have failed\n\n";
+                break;
 
+            case 4:
+                cout << " you have failed\n\n";
+                break;
 
+            case 3:
+                cout << " you have failed\n\n";
+                break;
 
+            case 2:
+                cout << " you have failed\n\n";
+                break;
+
+            case 1:
+                cout << " you have failed\n\n";
+                break;
+
+            case 0:
+                cout << " you have failed\n\n";
+                break;
+
+            default: cout << " error";
 
 
         }
